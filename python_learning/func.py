2fcfb534d6340f9f0ac31bed8493d103b696e72f
Sivani Marepalli <151424152+sivanimarepalli@users.noreply.github.com>
2026-03-13 11:05:22 +0530

    Add files via upload

---
+++ python_learning/func.py
@@ -0,0 +1,10 @@
+def happy_birthday(name): # the name in paranthesis is known as name
+    print("Happy birthday to you!")
+    print("You are old!!")
+    print("Happy birthday to you!")
+    print(f"Happy birthday to {name}")
+    print()
+happy_birthday("ABC") # we can call a function as many times as we want
+happy_birthday("Sivani") # we passed a value to the parameter we passed to the happy_birthday() function
+# A function can have multiple arguments but we have  to call the function only after passing all the value
+# The position of arguments and values matters.
---
+++ python_learning/funct1.py
@@ -0,0 +1,6 @@
+def display_invoice(username,amount,due_date):
+    print(f" Hello {username}")
+    print(f"Your bill of ${amount:.2f} is due:{due_date}")
+
+
+display_invoice("Sivani", 15000,"15-03-26")      
